b4b17c6ee68a0b6841e35524e3a5c250a1d020e1
Daniel <danielkrokovsky@hotmail.com>
2024-02-03 22:41:59 -0300

    ajustee

--- licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
+++ licensing-service/src/main/java/com/optimagrowth/license/LicensingServiceApplication.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @RefreshScope
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LicensingServiceApplication {
 
